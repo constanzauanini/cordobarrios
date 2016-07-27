f7a83fd60d7701fc94121d16de8dd7bc4ec015ea
Constanza Uanini <constanza.uanini@globant.com>
2016-07-27 00:03:07 -0300

    Paginas de barrios

--- app/scripts/controllers/barrioJardin.js
+++ app/scripts/controllers/barrioJardin.js
@@ -9,40 +9,48 @@
  */
 angular.module('cordoBarrios5App')
   .controller('BarrioJardinCtrl', function () {
-var negocios=[ { name:'Uthapizza',
-                  image: 'images/uthapizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                  comment: '' },
-                  { name:'Zucchipakoda',
-                  image: 'images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                   price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-                  comment: '' },
-                  { name:'Vadonut',
-                  image: 'images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-                  comment: '' },
-                  { name:'ElaiCheese Cake',
-                  image: 'images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                  comment: '' },
-                  { name:'Milanesas',
-                  image: 'images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                  comment: '' }  ];
+var negocios=[ 
+
+{ name:'Carnicería Sur',
+                  image: 'images/carinceria.jpg',
+                  description:'Encontra todos los cortes y la mejor calidad de carnes en Carniceria Cordoba. Ademas los mejores precios y la mejor ubicacion. Recibimos todos los medios de pago',
+                  address: 'Cruz Roja 5000',
+                  valoration: '80'  },
+
+
+{ name:'Verduleria Kiko',
+                  image: 'images/carinceria.jpg',
+                  description:'Encontra las mejores verduras!. Ademas los mejores precios y la mejor ubicacion. Recibimos todos los medios de pago',
+                  address: 'La entente 5000',
+                  valoration: '70'  },
+
+{ name:'Despensa Jardin',
+                  image: 'images/kiosco.jpg',
+                  description:'En despensa Jardin podras encontrar todo lo que buscas, todo tipo de bebidas, alimentos, articulos de limpieza y de higiene',
+                  address: 'Richieri 222',
+                  valoration: '30'
+                },
+                  { name:'Veterinaria Espionosa',
+                  image: 'images/veterinaria.jpg',
+                  description:'La mejor veterinaria !!! Tenemos todo tipo de alimentos, juguetes, accesorios para tu mascota',
+                  address: 'Botafogo 100',
+                  valoration: '10' },
+                  
+                  { name:'Despensa Angel',
+                  image: 'images/kiosco.jpg',
+                  description:'En despensa Angel podras encontrar todo lo que buscas, todo tipo de bebidas, alimentos, articulos de limpieza y de higiene',
+                  address: 'Leopoldo Lugones 22',
+                  valoration: '70'
+                },
+                  { name:'Guau Pet Shop',
+                  image: 'images/veterinaria.jpg',
+                  description:'La mejor veterinaria de Nueva Cordoba. Tenemos todo tipo de alimentos, juguetes, accesorios para tu mascota',
+                  address: 'Estrada 221',
+                  valoration: '70' },
+                  { name:'Carnicería Córdoba',
+                  image: 'images/carinceria.jpg',
+                  description:'Encontra todos los cortes y la mejor calidad de carnes en Carniceria Cordoba. Ademas los mejores precios y la mejor ubicacion. Recibimos todos los medios de pago',
+                  address: 'Transito Caceres 540',
+                  valoration: '50'  }  ];
   	this.negocios = negocios;
   });
